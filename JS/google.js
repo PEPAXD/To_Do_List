84ffcd9bef04171aa03a81477c3948d31204761d
Mauro Exequiel Pepa <mauropepa1997oficial@gmail.com>
2023-12-12 04:07:23 -0300

    Add Google login functionality

---
+++ JS/google.js
@@ -0,0 +1,23 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+import { auth } from './firebase.js';
+import { showErrorToast } from '../JS/Toastify.js'
+
+const googleButton = document.querySelector('#googleLogin');
+
+googleButton.addEventListener('click', async (e) => {
+    e.preventDefault();
+    const provider = new GoogleAuthProvider();
+    googleButton.disabled = true;
+
+    try {
+        const result = await signInWithPopup(auth, provider);
+        window.location.href = "./userValidated.html";
+
+    } catch (error) {
+        showErrorToast('Ocurrió un error durante el registro');
+
+    } finally {
+        googleButton.disabled = false;
+    }
+});
